3998c661e44e1545ace42aae74554e9c8cf8b48e
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-07-15 16:14:46 +0300

    feat: add first domain entity

---
+++ Auth/AuthDomain/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthDomain
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MinLength(3)]
+        [Required(AllowEmptyStrings = false)]
+        public required string FirstName { get; set; }
+
+        [MinLength(3)]
+        [Required(AllowEmptyStrings = false)]
+        public required string LastName { get; set; }
+
+        public string? Patronymic { get; set; }
+
+        public required string Password { get; set; }
+
+        public required string Login { get; set; }
+    }
+}
