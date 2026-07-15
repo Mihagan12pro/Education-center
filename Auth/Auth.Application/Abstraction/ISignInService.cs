c63fdd6a5955028a32de5264834782a47fba83b8
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-07-15 17:01:06 +0300

    some changes

--- Auth/Auth.Application/Abstraction/ISignInService.cs
+++ Auth/Auth.Application/Abstraction/ISignInService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Auth.Application.Abstraction
+﻿namespace Auth.Application.Abstraction
 {
     public interface ISignInService
     {
